922c2bd22769ddf17fa8d470092d0fad36d5b0b3
Kevin Arce <iav.kevinarce@ufg.edu.sv>
2022-05-27 08:06:30 -0600

    Implementing Sanctum to authenticate users

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
